50c9a867e4e934a3411216702156b5e24634b2bb
Roger Leal <roger.mo.leal@gmail.com>
2026-04-15 15:28:26 -0300

    fix(FEAT08): ajusta estilo do css para exibir as informações corretamente

--- sw.js
+++ sw.js
@@ -15,7 +15,10 @@ self.addEventListener("install", event => {
 self.addEventListener("fetch", event => {
   event.respondWith(
     fetch(event.request).catch(() => {
-      return caches.match(OFFLINE_URL);
+      if (event.request.mode === "navigate") {
+        return caches.match(OFFLINE_URL);
+      }
+      return new Response("", { status: 404 });
     })
   );
-}); 
+});
